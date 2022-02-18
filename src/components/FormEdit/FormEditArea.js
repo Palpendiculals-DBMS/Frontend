3498853dd276227e9581e1e88a03dee3f89c6961
TahirReyaz <tahir200213@st.jmi.ac.in>
2022-02-18 18:42:20 +0530

    Added edit and analytics button and changed some styles

--- src/components/FormEdit/FormEditArea.js
+++ src/components/FormEdit/FormEditArea.js
@@ -1,40 +1,35 @@
-import React, { useState } from 'react'
-import { FormEditContext } from '../../pages/form/FormEdit'
+import React, { useState } from "react";
+import { FormEditContext } from "../../pages/form/FormEdit";
 
-import DisplayEditableQuestions from './components/DisplayEditableQuestions';
+import DisplayEditableQuestions from "./components/DisplayEditableQuestions";
 
-import Input from './components/Basic/Input';
-import Textarea from './components/Basic/Textarea';
+import Input from "./components/Basic/Input";
+import Textarea from "./components/Basic/Textarea";
 
 function FormEditArea() {
+  const { formData, formDataActions } = React.useContext(FormEditContext);
 
-    const { formData, formDataActions } = React.useContext(FormEditContext);
+  return (
+    <div className={`h-3/5 overflow-y-scroll mt-5 shadow-xl`}>
+      <div className={`bg-white p-10`}>
+        <Input
+          className={`w-1/2 p-3 text-xl`}
+          value={formDataActions.getFormInfo().title}
+          placeholder="Enter Form Title"
+        />
 
-    return (
-        <div className={`w-full mt-10 ml-10 shadow-xl`}>
-            <div className={`bg-white p-10`}>
-                <Input
-                    className={`w-1/2 p-3 text-xl`}
-                    value={formDataActions.getFormInfo().title}
-                    placeholder='Enter Form Title'
-                />
+        <Textarea
+          className={`w-3/4 p-1 mt-10 text-sm h-24 resize-none`}
+          value={formDataActions.getFormInfo().description}
+          placeholder="Enter Form Description"
+        />
 
+        <hr className={`mt-2 border-red-500 border-t-2`} />
 
-                <Textarea
-                    className={`w-3/4 p-1 mt-10 text-sm h-24 resize-none`}
-                    value={formDataActions.getFormInfo().description}
-                    placeholder='Enter Form Description'
-                />
-
-                <hr
-                    className={`mt-2 border-red-500 border-t-2`}
-                />
-
-                <DisplayEditableQuestions />
-
-            </div>
-        </div>
-    )
+        <DisplayEditableQuestions />
+      </div>
+    </div>
+  );
 }
 
-export default FormEditArea
+export default FormEditArea;
