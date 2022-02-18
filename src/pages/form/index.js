3554e704c4a735f909f43941e6333dfaa65bf4b0
TahirReyaz <tahir200213@st.jmi.ac.in>
2022-02-18 18:41:41 +0530

    Added analytics page and its route

--- src/pages/form/index.js
+++ src/pages/form/index.js
@@ -1,40 +1,36 @@
-import React, { useEffect } from 'react'
+import React, { useEffect } from "react";
 import Layout from "../../components/Layout/Layout";
 
 import { Switch, Route, useHistory } from "react-router-dom";
-import Dashboard from './Dashboard'
-import Form from './FormEdit';
-import FormDisplay from './FormDisplay'
+import Dashboard from "./Dashboard";
+import Form from "./FormEdit";
+import FormDisplay from "./FormDisplay";
 
-import { useSelector } from 'react-redux'
+import { useSelector } from "react-redux";
+import FormAnalytics from "./FormAnalytics";
 
 function Index() {
+  const auth = useSelector((state) => state.auth);
+  const history = useHistory();
+  useEffect(() => {
+    if (!auth.isAuthenticated) {
+      history.push("/");
+    }
+  }, [auth]);
 
-    const auth = useSelector((state) => state.auth);
-    const history = useHistory();
-    useEffect(() => {
-        if (!auth.isAuthenticated) {
-            history.push('/');
-        }
-    }, [auth]);
+  return (
+    <>
+      <Layout>
+        <Switch>
+          <Route path="/form/dashboard" component={Dashboard} />
 
-    return (
-        <>
-            <Layout>
-                <Switch>
-                    <Route
-                        path="/form/dashboard"
-                        component={Dashboard}
-                    />
+          <Route path="/form/edit/:id" component={Form} />
 
-                    <Route
-                        path="/form/edit/:id"
-                        component={Form}
-                    />
-                </Switch>
-            </Layout>
-        </>
-    )
+          <Route path="/form/anal/:id" component={FormAnalytics} />
+        </Switch>
+      </Layout>
+    </>
+  );
 }
 
-export default Index
+export default Index;
