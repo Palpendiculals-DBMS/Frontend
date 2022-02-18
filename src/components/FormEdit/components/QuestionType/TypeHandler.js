3ed93e20331ee37c90ef014c4eb45f50c1711dea
TahirReyaz <tahir200213@st.jmi.ac.in>
2022-02-18 19:02:35 +0530

    Added longText component, and functionality of long text button in form edit

--- src/components/FormEdit/components/QuestionType/TypeHandler.js
+++ src/components/FormEdit/components/QuestionType/TypeHandler.js
@@ -1,24 +1,27 @@
-import React from 'react'
-import ShortText from './ShortText';
+import React from "react";
+import ShortText from "./ShortText";
+import LongText from "./LongText";
 
 function TypeHandler({ question, index }) {
+  switch (question.type) {
+    case "shorttext":
+      return (
+        <React.Fragment>
+          <ShortText question={question} index={index} />
+        </React.Fragment>
+      );
+    case "longtext":
+      return (
+        <React.Fragment>
+          <LongText question={question} index={index} />
+        </React.Fragment>
+      );
+      break;
+    default:
+      break;
+  }
 
-    switch (question.type) {
-        case 'shorttext':
-            return (
-                <React.Fragment>
-                    <ShortText question={question} index={index} />
-                </React.Fragment>
-            )
-        case 'longtext':
-            break;
-        default:
-            break;
-    }
-
-    return (
-        <></>
-    )
+  return <></>;
 }
 
-export default TypeHandler
+export default TypeHandler;
